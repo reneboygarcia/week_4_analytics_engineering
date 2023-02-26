f7df75c6535a5494b93e78761d5ccf150b6cae85
reneboygarcia <reneboygarcia@mgmail.com>
2023-02-26 20:59:57 +0800

    add docker

--- prefect_nytaxi_deploy_wk4.py
+++ prefect_nytaxi_deploy_wk4.py
@@ -2,24 +2,29 @@
 # import
 from prefect.filesystems import GitHub
 from prefect.deployments import Deployment
+from prefect.infrastructure.docker import DockerContainer
 from etl_web_to_bq_wk4 import parent_etl_web_to_bq
 
 # Fetch storage from GitHub
 github_block = GitHub.load("ny-taxi-github-block-wk4")
+# Set-up Infrastructure
+docker_block = DockerContainer.load("prefect-docker-block")
+
 
 # https://docs.prefect.io/api-ref/prefect/deployments/#prefect.deployments.Deployment.build_from_flow
 
-gsc_git_dep = Deployment.build_from_flow(
+bq_git_dep = Deployment.build_from_flow(
     flow=parent_etl_web_to_bq,
     name="ny-taxi-flow-wk4",
     storage=github_block,
+    infrastructure=docker_block,
 )
 
-print("Successfully deployed NY Taxi Github Block. Check app.prefect.cloud")
+print("Successfully deployed NY Taxi Github Block with Docker. Check app.prefect.cloud")
 
 # execute
 if __name__ == "__main__":
-    gsc_git_dep.apply()
+    bq_git_dep.apply()
 
 # to deploy
 # prefect deployment run etl-parent-to-gcs/ny-taxi-flow-wk3 --params '{"year":2019, "months": [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1]}'
